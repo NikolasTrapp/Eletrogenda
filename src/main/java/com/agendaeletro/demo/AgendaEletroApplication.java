bc6885a76a13e3fe75aec3bb6157cfce63c6ba57
Nikolas Trapp <nikolastrapp@gmail.com>
2022-08-12 18:09:04 -0300

    first commit

---
+++ src/main/java/com/agendaeletro/demo/AgendaEletroApplication.java
@@ -0,0 +1,13 @@
+package com.agendaeletro.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaEletroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaEletroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agendaeletro/demo/AgendaEletroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendaeletro.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaEletroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
