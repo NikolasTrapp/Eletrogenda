aec1270be8282d55e5c06fff2bd6796b4ecf54d5
Nikolas Trapp <nikolastrapp@gmail.com>
2022-08-13 22:48:48 -0300

    Concluindo a camada de resources e iniciando templates

--- src/main/java/com/agendaeletro/project/resources/SchedulingResources.java
+++ src/main/java/com/agendaeletro/project/resources/SchedulingResources.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.agendaeletro.project.services.SchedulingService;
 import com.agendaeletro.project.services.TeacherService;
 
 @RestController // Anotação para definir que esta classe é uma classe controladora
+@CrossOrigin("*")
 @RequestMapping(value = "/schedulings")
 public class SchedulingResources {
 
